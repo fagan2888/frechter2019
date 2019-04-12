52a59c0c415a18556f285a855aefcf2a2303c61c
Sina Tootoonian <sina.tootoonian@gmail.com>
2019-04-12 17:23:52 +0100

    Minor refactoring and commenting.

--- prepare_data.py
+++ prepare_data.py
@@ -6,20 +6,22 @@ import rpy2.robjects as robjects
 
 data_dir = "./input_data"
 
-# LOAD DATA ABOUT ODOURS AND GROUPS
 print("Loading odour names and groups.")
 robjects.r['load'](op.join(data_dir, "stats_by_class_mat36_python.RData"))
 odour_names  = list(robjects.r["odour_names"])
 odour_groups = list(robjects.r["odour_groups"])
-bad_groups      = ['Mix','Blank','min_acid']
-good_odour_inds = [i for i, g in enumerate(odour_groups) if g not in bad_groups]
-odour_names  = [odour_names[i]  for i in good_odour_inds]
-odour_groups = [odour_groups[i] for i in good_odour_inds]
+bad_groups   = ['Mix','Blank','min_acid']
+# Keep all odours whose group isn't one of the bad groups
+odour_names  = [name  for i, name  in enumerate(odour_names) if odour_groups[i] not in bad_groups]
+# Keep all the groups which are not one of the bad groups
+odour_groups = [group for group in odour_groups if group not in bad_groups]
 
-## LOAD THE SPIKE COUNTS DATA
-print("Loading spike counts data.")
+# Load the table of cells we're going to use
+print("Loading the physplit table.")
 db = pandas.read_csv(op.join(data_dir, "physplit.csv"))
-data_file = op.join(data_dir, "spike_counts_per_trial.csv")
+
+print("Loading spike counts data.")
+data_file  = op.join(data_dir, "spike_counts_per_trial.csv")
 count_data = pandas.read_csv(data_file)
 # The count data is for more cells and odours than we need.
 # So subset the data for the odours we need...
@@ -28,29 +30,28 @@ count_data = count_data.loc[count_data['odour'].isin(odour_names)]
 count_data = count_data.loc[count_data["cell"].isin(set(db["cell"].values))] 
 cells = set(count_data["cell"])
 
-## FILTER OUT THE CELLS WITHOUT THE RIGHT AMOUNT OF DATA
 print("Filtering out cells with incorrectly shaped data.")
-required_shape = (6120, 3) # 6120 = 30 odors * 51 bins * 4 trials
+required_shape  = (6120, 3) # 6120 = 30 odors * 51 bins * 4 trials
 cell_validity   = {cell:count_data[count_data["cell"] == cell][["trial","bin","odour"]].values.shape == required_shape for cell in cells}
 valid_cells   = {cell for cell,valid in cell_validity.items() if valid}
 invalid_cells = cells - valid_cells
-print("{} valid cells".format(len(valid_cells)))
-print("{} invalid cells: {}".format(len(invalid_cells), invalid_cells))
+print("{} valid   cells.".format(len(valid_cells)))
+print("{} invalid cell(s): {}".format(len(invalid_cells), invalid_cells))
 
-## SUBSET TO THE VALID CELLS
 print("Subsetting to valid cells.")
 count_data_valid = count_data.loc[count_data["cell"].isin(valid_cells)]
 class_information_present = all([len(x)>0 for x in db.loc[db["cell"].isin(valid_cells)]["class"].values])
 if not class_information_present:
     raise Exception("Class information was not present for all cells.")
 
-## RESHAPE THE BINNED SPIKETIMES DATA INTO A BINS x TRIALS x ODOURS x CELLS (btoc) array
 print("Reshaping spike counts data into array.")
-counts     = count_data_valid.sort_values(by=["cell","odour", "trial", "bin"])["count"].values
-btoc       = np.reshape(counts, (51, 4, len(odour_names), len(valid_cells)), order="F") # bins, trial, odors, cells
-btoc_cells = sorted(list(set(count_data_valid["cell"])))
-btoc_odours= sorted(list(set(count_data_valid["odour"])))
-btoc_groups= [odour_groups[odour_names.index(n)] for n in btoc_odours]
+# Sort the count table by the columns so that we can pull out the cell and odour names in the correct order,
+# and so that the trials and bins are in the correct order.
+counts      = count_data_valid.sort_values(by=["cell","odour", "trial", "bin"])["count"].values
+btoc        = np.reshape(counts, (51, 4, len(odour_names), len(valid_cells)), order="F") # bins, trial, odors, cells
+btoc_cells  = sorted(list(set(count_data_valid["cell"])))  
+btoc_odours = sorted(list(set(count_data_valid["odour"])))
+btoc_groups = [odour_groups[odour_names.index(odour)] for odour in btoc_odours]
 
 # Write the btoc data to disk
 print("Writing spike counts data to disk.")
